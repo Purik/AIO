3e9a27bcd6a6c98dce2806e1de2fee7642f4402a
pavel <minikspb@gmail.com>
2018-10-31 14:59:35 +0300

    new Demos

---
+++ Demos/HowTo/HowTo.LocalContexts.dpr
@@ -0,0 +1,20 @@
+program HowTo.LocalContexts;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  {$I ../Impl.inc}
+  {$I ../Includes.inc}
+  System.SysUtils;
+
+begin
+  Greenlets.Context()
+  try
+    { TODO -oUser -cConsole Main : Insert code here }
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
